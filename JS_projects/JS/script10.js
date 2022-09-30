4effd15f8e757c5088432d578805109d28deef6b
Mounir <abciremounir47@gmail.com>
2022-09-30 22:53:04 +0100

    modified the script10

--- JS_projects/JS/script10.js
+++ JS_projects/JS/script10.js
@@ -3,20 +3,62 @@ const right = document.getElementById("right");
 const left = document.getElementById("left");
 var num = 0;
 
+right.style.opacity = "0.2";
 left.addEventListener("click", img_slider);
-
 function img_slider() {
-  img[num].classList.remove("active");
-  img[num].classList.add("activeLeft");
+  if (num == 4) {
+    left.style.opacity = "0.2";
+    right.style.opacity = "1";
+    return false;
+  } else {
+    left.style.opacity = "1";
+    right.style.opacity = "1";
+    img[num].classList.remove("active");
+    img[num].classList.add("activeLeft");
 
-  num++;
-  img[num].classList.remove("activeRight");
-  img[num].classList.add("active");
+    num++;
+    img[num].classList.remove("activeRight");
+    img[num].classList.add("active");
+  }
 
-  num++;
-  img[num].classList.remove("notActive");
-  img[num].classList.add("activeRight");
-  num--;
+  if (num == 4) {
+    left.style.opacity = "0.2";
+    right.style.opacity = "1";
+    return false;
+  } else {
+    left.style.opacity = "1";
+    right.style.opacity = "1";
+    num++;
+    img[num].classList.remove("notActive");
+    img[num].classList.add("activeRight");
 
-  console.log(num);
+    num--;
+  }
+}
+
+right.addEventListener("click", img_slider2);
+function img_slider2() {
+  if (num == 0) {
+    left.style.opacity = "1";
+    right.style.opacity = "0.2";
+    return false;
+  } else {
+    right.style.opacity = "1";
+    left.style.opacity = "1";
+    img[num].classList.remove("active");
+    img[num].classList.add("activeRight");
+
+    num--;
+    img[num].classList.remove("activeLeft");
+    img[num].classList.add("active");
+  }
+
+  if (num == 0) {
+    left.style.opacity = "1";
+    right.style.opacity = "0.2";
+    return false;
+  } else {
+    left.style.opacity = "1";
+    right.style.opacity = "1";
+  }
 }
