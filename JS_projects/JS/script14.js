af12b634778978adf3ac8100e47793c38d80d859
MounirDevWeb <abciremounir@gmail.com>
2022-10-22 14:41:08 +0100

    finished the 14 th project

---
+++ JS_projects/JS/script14.js
@@ -0,0 +1,36 @@
+const ChackNavbar = document.getElementById("checkNavbar");
+const ul = document.querySelector("ul");
+const li = document.querySelectorAll("li");
+let count = 1;
+
+ChackNavbar.style.transform = "rotate(226deg)";
+ChackNavbar.onclick = () => {
+    if (count == 0) {
+        count++;
+        ChackNavbar.style.transform = "rotate(226deg)"
+        for (let i = 0; i < li.length; i++) {
+            li[i].style.transform = "translateY(-50px)";
+        }
+        setTimeout(() => {
+            ul.style.width = 10 + "px";
+        }, 500)
+        setTimeout(() => {
+            ul.style.bottom = 27.5 + "px";
+        }, 1000)
+
+    }
+    else {
+        count--;
+        ChackNavbar.style.transform = "rotate(0deg)"
+        ul.style.bottom = 0 + "px";
+        setTimeout(() => {
+            ul.style.width = 80 + "%";
+        }, 500)
+        for (let i = 0; i < li.length; i++) {
+            setTimeout(() => {
+                li[i].style.transform = "translateY(0px)";
+            }, 1000)
+        }
+    }
+
+}
